0272595de957cd674b4ef4d2110151ccc2fc34ee
powtzulist <delasanarwin@gmail.com>
2024-02-28 10:31:09 +0800

    login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
 import { UserWrapper } from '@/context/UserContext';
 
 const inter = Inter({ subsets: ["latin"] });
