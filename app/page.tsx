35c40f2f142f03172db0dec500cfb9a18e2617eb
powtzulist <delasanarwin@gmail.com>
2024-02-28 11:36:45 +0800

    Styled the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+
 
 export default function Home() {
+
   return (
-    <main className="flex justify-center items-center h-full text-white">
-      <button className="border-2 p-2">Log In with Google</button>
-    </main>
+    <main className="flex flex-col justify-center items-center h-full bg-center text-white bg-[url('../assets/icons/LandPageBG.svg')] " >
+      <h1 className="font-bold text-7xl mb-8">Welcome to Beacon</h1>
+      <button className="flex flex-row border-2 p-2 gap-x-2" >Log In with Google <img src="https://fonts.gstatic.com/s/i/productlogos/googleg/v6/24px.svg" /> </button >
+    </main >
   );
 }
