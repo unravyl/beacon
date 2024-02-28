b48ad3e72ea4ee36c4155c02f70b6ddc85c11fb2
powtzulist <delasanarwin@gmail.com>
2024-02-28 13:06:35 +0800

    Made the Profile Page

---
+++ app/profile/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Login from "../../components/Profile.jsx"
+
+function page() {
+    return (
+        <section>
+            Profile
+            <div>
+                <Login />
+            </div>
+        </section>
+    );
+}
+
+export default page;
