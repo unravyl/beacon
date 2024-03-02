feeb4a97686a64abd2a30eff88e39479c822cf9b
Sheldon Sagrado <sheldon@bposeats.com>
2024-03-02 17:35:59 +0800

    refactor landing page and reorganize files

--- app/page.tsx
+++ app/page.tsx
@@ -27,17 +27,22 @@ export default function Home() {
   }, [hasAccount, router, hasAccountData]);
 
   return (
-    <main className="flex flex-col items-center h-full bg-center text-white">
+    <main className="flex items-center justify-center h-full text-white">
+      <div className="flex flex-col items-center justify-center h-full">
+        <img src="/generics/logo.png" className="w-[15rem]" />
+        <h1 className="font-bold text-5xl text-center mb-8 sm:text-7xl">
+          Welcome to Beacon!
+        </h1>
+        <button
+          className="flex border-2 py-3 px-4 gap-x-2 hover:bg-slate-700 rounded"
+          onClick={handleLoginButtonClick}
+        >
+          Log In with Google <img src="/generics/google-logo.svg" />{' '}
+        </button>
+      </div>
+
       {isLoading && <Spinner />}
-      <img src="/logo.png" className="w-[15rem] mt-24" />
-      <h1 className="font-bold text-7xl mb-8">Welcome to Beacon!</h1>
-      <button
-        className="flex flex-row border-2 p-2 gap-x-2 hover:bg-slate-400"
-        onClick={handleLoginButtonClick}
-      >
-        Log In with Google{' '}
-        <img src="https://fonts.gstatic.com/s/i/productlogos/googleg/v6/24px.svg" />{' '}
-      </button>
     </main>
   );
 }
+
