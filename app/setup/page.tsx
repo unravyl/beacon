41d34bd3dfe191afb0ed7a32e1a40a40c28b9b71
sheldon sagrado <sheldon@bposeats.com>
2024-03-04 16:55:54 +0800

    structure for setup page

---
+++ app/setup/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SetupForm from '@/components/setup/SetupForm';
+
+function page() {
+    return (
+        <section>
+            <SetupForm />
+        </section>
+    );
+}
+
+export default page;
---
+++ components/setup/SetupForm.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useEffect, useState } from 'react';
+
+function SetupForm() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 h-dvh text-white">
+            <main>
+            </main>
+            <aside className="bg-[#C7D6E5]">
+            </aside>
+        </div>
+    )
+}
+
+export default SetupForm;
