b63f13ee6076530530ff8f24c74e7bcb174cec55
Sheldon Sagrado <sheldon@bposeats.com>
2024-03-09 18:02:47 +0800

    setup page prototype 1

---
+++ components/setup/FormQuestions.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+function FormQuestions(props) {
+  const { formItems, setFormItems, questionNumber } = props;
+  const [inputText, setInputText] = useState('');
+
+  // methods
+  const addAnswer = () => {
+    if (!inputText) return;
+    const newAnswers = [...formItems[questionNumber].answers, inputText];
+    setFormItems((prevFormItems) => {
+      const updatedFormItems = [...prevFormItems];
+      updatedFormItems[questionNumber].answers = newAnswers;
+      return updatedFormItems;
+    });
+
+    setInputText('');
+  };
+
+  const removeAnswer = (index) => {
+    const newAnswers = formItems[questionNumber].answers;
+    newAnswers.splice(index, 1);
+    setFormItems((prevFormItems) => {
+      const updatedFormItems = [...prevFormItems];
+      updatedFormItems[questionNumber].answers = newAnswers;
+      return updatedFormItems;
+    });
+  };
+
+  return (
+    <div>
+      <h3 className="text-[30px] font-bold leading-none">
+        {formItems[questionNumber].question}
+      </h3>
+      <p className="opacity-80 text-[12px] font-light mb-1">
+        {formItems[questionNumber].description}
+      </p>
+
+      <div className="flex items-center my-2">
+        <input
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              addAnswer();
+            }
+          }}
+          type="text"
+          placeholder="Type your answer here..."
+          className="w-full my-1 h-[40px] p-2 pe-10 rounded-md shadow-md text-accent"
+        />
+        {inputText.length > 0 && (
+          <div className="relative">
+            <button
+              onClick={addAnswer}
+              className="absolute translate-x-[-120%] translate-y-[-50%] bg-accent text-white px-2 py-2 text-[10px] rounded-md hover:brightness-150 shadow-sm"
+            >
+              <i className="bx bx-plus" />
+            </button>
+          </div>
+        )}
+      </div>
+
+      <div className="flex flex-wrap gap-2">
+        {formItems[questionNumber].answers.map((item, index) => (
+          <div
+            className="flex items-center gap-1 text-[12px] bg-white rounded-full px-2.5 py-1"
+            key={index}
+          >
+            {item}
+            <button
+              onClick={() => removeAnswer(index)}
+              className="flex items-center rounded-full hover:bg-red-300"
+            >
+              <i className="bx bx-x" />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default FormQuestions;
+
--- components/setup/SetupForm.tsx
+++ components/setup/SetupForm.tsx
@@ -1,15 +1,177 @@
-"use client";
+'use client';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+// components
+import FormQuestions from './FormQuestions';
 
 function SetupForm() {
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-2 h-dvh text-white">
-            <main>
-            </main>
-            <aside className="bg-[#C7D6E5]">
-            </aside>
+  const instructionList = [
+    {
+      title: 'Set Up Your Data',
+      description: "Input all necessary information for Beacon's AI analysis",
+    },
+    {
+      title: 'Generate Career Graph',
+      description:
+        'Beacon will craft a visual career roadmap tailored specifically to you',
+    },
+    {
+      title: 'Explore Job Descriptions',
+      description:
+        'Interact with job description nodes to get detailed insights into different roles',
+    },
+    {
+      title: 'Expand Nodes',
+      description:
+        'By expanding a job description node, Beacon provides solutions on how to reach that specific job title',
+    },
+  ];
+
+  const [formItems, setFormItems] = useState([
+    {
+      question: 'Interest',
+      description:
+        'Briefly describe your interests, passions, and what motivates you in your career',
+      answers: ['UI/UX development'],
+    },
+    {
+      question: 'History',
+      description:
+        'Summarize your past work experiences, projects, and any significant milestones in your career journey.',
+      answers: [],
+    },
+    {
+      question: 'Strengths',
+      description:
+        'Identify your key strengths, skills, and attributes that contribute to your professional success',
+      answers: [],
+    },
+    {
+      question: 'Weaknesses',
+      description:
+        'Reflect on areas where you feel you may need improvement or development',
+      answers: [],
+    },
+    {
+      question: 'Education',
+      description:
+        'Outline your educational background, including degrees, certifications, and relevant courses',
+      answers: [],
+    },
+  ]);
+
+  const [clientInfo, setClientInfo] = useState({
+    name: 'Foo bar',
+    email: 'foobar@gmail.com',
+  });
+
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // computed
+  const isLastStep = questionNumber == formItems.length - 1;
+
+  // methods
+  const moveNext = () => {
+    setQuestionNumber(questionNumber + 1);
+  };
+
+  const movePrev = () => {
+    setQuestionNumber(questionNumber - 1);
+  };
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 h-dvh text-white">
+      <main className="flex flex-col px-10 py-6">
+        <header className="flex">
+          <Image
+            src="/generics/logo.png"
+            alt="beacon logo"
+            className="w-[80px] h-[80px] "
+            width={80}
+            height={80}
+          />
+          <div className="flex flex-col">
+            <h1 className="text-[48px] leading-none font-semibold mb-0.5">
+              Bea<span className="text-[#A2C5E8]">con</span>
+            </h1>
+            <p className="text-[16px] leading-none">
+              Your AI Career Guidance Application
+            </p>
+            <p className="text-[12px] leading-none font-extralight">
+              Creating Personalized Career Roadmaps for Everyone
+            </p>
+          </div>
+        </header>
+        <section className="flex flex-col flex-grow justify-center pb-[80px] gap-6 pl-6">
+          {instructionList.map((item, index) => (
+            <div className="flex gap-4 items-center" key={'item-' + index}>
+              <div className="w-[25px] h-[25px] min-w-[25px] min-h-[25px] flex justify-center bg-[#C7D6E5] rounded-full">
+                <div className="flex flex-col justify-center">
+                  <p className="text-[#0C1323] text-[16px] font-bold">
+                    {index + 1}
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col">
+                <h2 className="text-[#C7D6E5] text-[16px] font-bold">
+                  {item.title}
+                </h2>
+                <p className="text-[14px]">{item.description}</p>
+              </div>
+            </div>
+          ))}
+        </section>
+      </main>
+
+      <aside className="text-[#0F172A] bg-[#DBEDFF] flex flex-col gap-5 p-20">
+        <div>
+          <h2 className="text-[40px] font-bold leading-none mb-1">
+            Setup your profile!
+          </h2>
+          <p className="text-[16px] font-medium leading-none opacity-80">
+            Name: {clientInfo.name} | Email: {clientInfo.email}
+          </p>
         </div>
-    )
+        <div className="flex-grow flex flex-col justify-center pb-20 gap-5">
+          <FormQuestions
+            formItems={formItems}
+            setFormItems={setFormItems}
+            questionNumber={questionNumber}
+          />
+          <div className="flex gap-1">
+            {questionNumber > 0 && (
+              <button
+                onClick={movePrev}
+                className="bg-white rounded-md shadow-md px-4 py-3 flex items-center hover:bg-opacity-50"
+              >
+                <i className="bx bx-chevrons-left" />
+                back
+              </button>
+            )}
+            {questionNumber < formItems.length - 1 && (
+              <button
+                onClick={moveNext}
+                className="bg-gradient-to-r from-accent via-accent to-secondary text-white py-2 px-4 rounded-md shadow-md px-4 py-3 flex items-center hover:brightness-150"
+              >
+                next
+                <i className="bx bx-chevrons-right" />
+              </button>
+            )}
+          </div>
+          <div>
+            {isLastStep && (
+              <button className="bg-gradient-to-r from-accent via-accent to-secondary text-white py-2 px-4 rounded-md shadow-md px-6 py-3 blink">
+                Generate Custom Roadmap
+              </button>
+            )}
+          </div>
+        </div>
+      </aside>
+    </div>
+  );
 }
 
 export default SetupForm;
+
