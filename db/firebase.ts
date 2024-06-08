326583d56292a8cbea69110e04d756efcbd27aec
Maxell Milay <milaymaxell@gmail.com>
2024-06-08 09:24:36 +0800

    [Setup][Maxell] transferred secret data into env

--- db/firebase.ts
+++ db/firebase.ts
@@ -1,14 +1,15 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAVY1uo6VZ7M3eXOc3d6hBtp5KfeJmsUi8',
-  authDomain: 'beacon-c4f50.firebaseapp.com',
-  projectId: 'beacon-c4f50',
-  storageBucket: 'beacon-c4f50.appspot.com',
-  messagingSenderId: '699052165978',
-  appId: '1:699052165978:web:9e3cd4062bf17193ccc163',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export default app;
+
