e78b44942c896a77dfc8b47c01c887247b3cfff7
powtzulist <delasanarwin@gmail.com>
2024-06-11 22:43:49 +0800

    removed state and replaced with peer modifier

--- app/api-key-setup/page.tsx
+++ app/api-key-setup/page.tsx
@@ -1,11 +1,9 @@
 'use client';
 import React from 'react';
-import { useState } from 'react';
 import Image from 'next/image';
 import { IoMdInformationCircleOutline } from 'react-icons/io';
 
 function Page() {
-  const [tooltipVisible, setTooltipVisible] = useState(false);
   return (
     <section>
       <div className="flex flex-col items-center justify-center h-screen mx-auto">
@@ -27,50 +25,45 @@ function Page() {
           <main className="w-full">
             <div className="flex justify-center items-center gap-x-1  mb-2">
               <h3 className="text-xl font-semibold">OpenAI API Key</h3>
-              <div
-                onMouseEnter={() => setTooltipVisible(true)}
-                onMouseLeave={() => setTooltipVisible(false)}
-                className="relative"
-              >
+              <div className="relative">
                 <IoMdInformationCircleOutline
                   size={20}
-                  className="cursor-pointer"
+                  className="cursor-pointer peer"
                 />
-                {tooltipVisible && (
-                  <div className="absolute bottom-0 left-5 mt-2 p-3 w-96 bg-white border border-black text-justify rounded shadow-lg z-10 text-xs ">
-                    <p className="text-sm mb-1">
-                      <strong>How to Get Your OpenAI API Key:</strong>
-                    </p>
-                    <ul className="list-disc list-inside space-y-2">
-                      <li>
-                        Visit the{' '}
-                        <a
-                          href="https://www.openai.com/"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="text-blue-500 underline"
-                        >
-                          OpenAI website
-                        </a>{' '}
-                        and sign up for an account if you don't have one, or log
-                        in if you already do.
-                      </li>
-                      <li>
-                        Go to the API section by clicking on the dashboard in
-                        the top-right corner and selecting "API keys" from the
-                        sidebar.
-                      </li>
-                      <li>
-                        Click on "Create new secret key" to generate a new API
-                        key.
-                      </li>
-                      <li>
-                        After generating, copy the key and paste it in the input
-                        field to use beacon services.
-                      </li>
-                    </ul>
-                  </div>
-                )}
+
+                <div className="invisible absolute bottom-0 left-5 peer-hover:visible  peer mt-2 p-3 w-96 bg-white border border-black text-justify rounded shadow-lg z-10 text-xs ">
+                  <p className="text-sm mb-1">
+                    <strong>How to Get Your OpenAI API Key:</strong>
+                  </p>
+                  <ul className="list-disc list-inside space-y-2">
+                    <li>
+                      Visit the{' '}
+                      <a
+                        href="https://www.openai.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-500 underline"
+                      >
+                        OpenAI website
+                      </a>{' '}
+                      and sign up for an account if you don't have one, or log
+                      in if you already do.
+                    </li>
+                    <li>
+                      Go to the API section by clicking on the dashboard in the
+                      top-right corner and selecting "API keys" from the
+                      sidebar.
+                    </li>
+                    <li>
+                      Click on "Create new secret key" to generate a new API
+                      key.
+                    </li>
+                    <li>
+                      After generating, copy the key and paste it in the input
+                      field to use beacon services.
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
             <input
